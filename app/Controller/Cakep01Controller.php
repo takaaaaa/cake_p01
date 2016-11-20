f7be71bd7d460fb74a780dc0d310f068292611e2
yoshidatakayo <anoutu@yahoo.co.jp>
2016-11-20 15:59:41 +0900

    ビューの練習

--- app/Controller/Cakep01Controller.php
+++ app/Controller/Cakep01Controller.php
@@ -6,11 +6,17 @@ class Cakep01Controller extends AppController {
     public $name = 'Cakep01';
     // モデルを指定しない
     public $uses = false;
-    // ビューを指定しない
-    public $autoRender = false;
+    // ビューを指定しない $autoRenderがfalseの場合は$autoLayoutもfalse
+    //public $autoRender = false;
 
     public function index() {
         echo "コントローラーの練習です。";
+        //$this->setAction("other");//other関数実行。url変更なし。
+        //$this->redirect("./other");//強制的にアクセス。外部サイトもok!
+    }
+
+    public function other() {
+        echo "別ページを作ります。";
     }
 }
 
---
+++ app/View/Cakep01/index.ctp
@@ -0,0 +1,2 @@
+<h1>Viewの練習です。</h1>
+<p><?php echo date('Y/m/d', time()); ?></p>
